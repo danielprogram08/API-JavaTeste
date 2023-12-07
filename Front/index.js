96f6161ec20bf08eb5a552fcbbb740591cada21e
danielprogram08 <danieldanielsilva08@gmail.com>
2023-12-07 00:56:24 -0300

    Front-end concluído!

---
+++ Front/index.js
@@ -0,0 +1,37 @@
+const formulario = document.querySelector("form");
+const Inome = document.querySelector(".nome");
+const Iemail = document.querySelector(".email");
+const Isenha = document.querySelector(".senha"); 
+
+function cadastrar() {
+    
+    fetch("http://localhost:8000/cadastrar",
+        {
+            
+            headers: {
+                'Accept': 'aplication/json',
+                'Content-Type': 'aplication/json'
+            },
+            method: "POST",
+            body: JSON.stringify({
+                nome: Inome.value,
+                email: Iemail.value,
+                senha: Isenha.value
+            })
+        })
+        .then(function (res) { console.log(res) })
+        .catch(function (res) { console.log(res) })
+};
+
+function limpar () {
+    Inome.value = "";
+    Iemail.value = "";
+    Isenha.value = "";
+};
+
+formulario.addEventListener('submit', function (event) {
+    event.preventDefault();
+    
+    cadastrar();
+    limpar();
+});
